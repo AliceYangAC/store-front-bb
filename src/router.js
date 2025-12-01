632b03fe257e023d465d40b48beb47ac65f2959a
AliceYangAC <yang0447@algonquinlive.com>
2025-11-30 19:56:34 -0500

    Refactor router configuration: remove name property from search route for consistency

--- src/router.js
+++ src/router.js
@@ -2,12 +2,13 @@ import { createWebHistory, createRouter } from "vue-router";
 import ProductList from "./components/ProductList";
 import ProductDetail from "./components/ProductDetail";
 import ShoppingCart from "./components/ShoppingCart";
+import SearchList from "./components/SearchList";
 
 const routes = [
   { path: "/", component: ProductList },
   { path: "/product/:id", component: ProductDetail },
   { path: "/cart", component: ShoppingCart },
-  { path: '/search', name: 'Search', component: SearchList },
+  { path: '/search', component: SearchList },
 ];
 
 const router = createRouter({
